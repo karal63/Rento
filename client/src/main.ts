21c99c3acfe80df148162c588b7b6c2f485fadbf
leo <kuzmichleva1@gmail.com>
2025-11-08 21:49:54 +0100

    intialize vietify

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createVuetify } from "vuetify";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+const app = createApp(App);
+
+app.use(vuetify);
+
+app.mount("#app");
