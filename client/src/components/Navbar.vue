db3ee13c5b66e73cfda013bec0a76a242dd4687c
leo <kuzmichleva1@gmail.com>
2025-11-09 18:07:52 +0100

    replace vuetify with normal syntax & tailwind

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,22 +1,67 @@
 <script setup lang="ts">
+    import { Icon } from '@iconify/vue';
+    import { ref } from 'vue';
     import { useI18n } from 'vue-i18n';
 
     const { t, locale } = useI18n();
+
+    const open = ref(false);
 </script>
 
 <template>
-    <v-app-bar flat app color="customWhite">
-        <div class="w-full max-w-[1800px] m-auto flex items-center px-4">
-            <!-- Left side -->
-            <v-toolbar-title>
-                <span class="text-2xl font-semibold">{{ t('app.name') }}</span>
-            </v-toolbar-title>
+    <nav class="my-5">
+        <div class="max-w-[1900px] m-auto flex items-center flex-between">
+            <h3 class="text-3xl">{{ t('app.name') }}</h3>
 
-            <v-spacer></v-spacer>
-            <!-- pushes button to the right -->
+            <div class="flex items-center gap-3">
+                <button
+                    class="inline-flex justify-center items-center rounded-md bg-transparent px-3 h-10 hover:bg-mainHoverDarkBg transition cursor-pointer"
+                >
+                    <Icon icon="fontisto:day-sunny" class="text-xl" />
+                </button>
 
-            <!-- Right side -->
-            <v-btn @click="locale = 'pl'" variant="flat" color="#5865f2">Log in</v-btn>
+                <!-- separate reusable UI element | move into shared dir -->
+                <div class="relative inline-block text-left">
+                    <!-- make fixed height or ask how to not set fixed height but have both elements same height -->
+                    <button
+                        @click="open = !open"
+                        class="inline-flex gap-1 justify-center items-center rounded-md bg-transparent px-5 h-10 hover:bg-mainHoverDarkBg transition cursor-pointer"
+                    >
+                        EN
+                        <Icon class="text-xl" icon="iconamoon:arrow-down-2-light" />
+                    </button>
+
+                    <transition name="fade">
+                        <div
+                            v-if="open"
+                            class="absolute right-0 z-10 mt-2 w-20 origin-top-right rounded-lg bg-mainHoverOnGray shadow-lg ring-1 ring-black ring-opacity-5"
+                        >
+                            <div class="py-1">
+                                <a href="#" class="block px-4 py-2 text-sm">PL</a>
+                                <a href="#" class="block px-4 py-2 text-sm">DE</a>
+                            </div>
+                        </div>
+                    </transition>
+                </div>
+
+                <!-- separate reusable UI element | move into shared dir -->
+                <button
+                    class="px-5 h-10 rounded-md bg-blue-500 hover:bg-blue-400 transition cursor-pointer"
+                >
+                    LOG IN
+                </button>
+            </div>
         </div>
-    </v-app-bar>
+    </nav>
 </template>
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.15s;
+    }
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+</style>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,30 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+    import { Icon } from '@iconify/vue';
+</script>
 
 <template>
-    <v-main color="customWhite">
-        <div class="w-full max-w-[1800px] m-auto flex items-center px-4">
-            <v-container style="width: 1400px" class="mt-28 flex gap-5">
-                <v-sheet width="50%">
-                    <h1 class="text-8xl font-extrabold">
-                        Premium
-                        <br />
-                        Car Rental
-                        <br />
-                        In Warsaw
-                    </h1>
-                    <p class="mt-10 text-xl text-gray-500">
-                        Here you’ll find the car you’ve always dreamed of — a thrilling and stylish
-                        ride that fits your budget without compromising on quality or value.
-                    </p>
-                    <v-btn class="mt-10" variant="flat" color="primary" size="x-large">
-                        Browse offers
-                        <v-icon class="ml-2">mdi-arrow-right</v-icon>
-                    </v-btn>
-                </v-sheet>
-                <v-sheet width="50%">
-                    <img src="../assets/car.png" alt="" />
-                </v-sheet>
-            </v-container>
+    <div>
+        <div class="max-w-[1500px] m-auto flex mt-36 gap-5 items-center px-4">
+            <div class="w-1/2">
+                <h1 class="text-8xl font-extrabold">
+                    Premium
+                    <br />
+                    Car Rental
+                    <br />
+                    In Warsaw
+                </h1>
+                <p class="mt-10 text-xl text-gray-400 leading-8 max-w-4/5">
+                    Here you’ll find the car you’ve always dreamed of — a thrilling and stylish ride
+                    that fits your budget without compromising on quality or value.
+                </p>
+
+                <button
+                    class="mt-10 inline-flex gap-2 justify-center items-center rounded-md text-lg tracking-widest bg-blue-500 px-7 h-14 hover:bg-blue-400 transition cursor-pointer"
+                >
+                    BROWSE OFFERS
+                    <Icon icon="material-symbols:search-rounded" class="text-xl" />
+                </button>
+            </div>
+            <div class="w-1/2">
+                <img src="../assets/car.png" alt="" />
+            </div>
         </div>
-    </v-main>
+    </div>
 </template>
