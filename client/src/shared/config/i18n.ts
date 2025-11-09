91022e82d2d7c8e4cc2e3d3fde51ef6363fbc32d
leo <kuzmichleva1@gmail.com>
2025-11-09 22:02:29 +0100

    refactor an app

--- client/src/shared/config/i18n.ts
+++ client/src/shared/config/i18n.ts
@@ -3,10 +3,10 @@ import { createI18n } from 'vue-i18n';
 function loadLocaleMessages() {
     const messages: Record<string, any> = {};
 
-    const locales = import.meta.glob('./lang/*.json', { eager: true });
+    const locales = import.meta.glob('../const/lang/*.json', { eager: true });
 
     for (const path in locales) {
-        const matched = path.match(/\.\/lang\/([A-Za-z0-9-_]+)\.json$/i);
+        const matched = path.match(/\.\/const\/lang\/([A-Za-z0-9-_]+)\.json$/i);
         if (matched && matched[1]) {
             const locale = matched[1];
             messages[locale] = (locales[path] as any).default || locales[path];
