be2cb1cb45ceafc66b977bf5cba9fbe74f59d115
leo <kuzmichleva1@gmail.com>
2025-11-10 17:20:09 +0100

    create brands bar

--- client/src/pages/landing/LandingPage.vue
+++ client/src/pages/landing/LandingPage.vue
@@ -2,38 +2,43 @@
     import { Icon } from '@iconify/vue';
     import { Button } from '../../shared/ui/button';
     import { useI18n } from 'vue-i18n';
+    import Brands from './Brands.vue';
 
     const { t, locale } = useI18n();
 </script>
 
 <template>
     <div>
-        <div class="max-w-[1500px] m-auto flex mt-36 gap-5 items-center px-4">
-            <div class="w-1/2">
-                <h1 class="text-8xl font-extrabold">
-                    Premium
-                    <br />
-                    Car Rental
-                    <br />
-                    In Warsaw
-                </h1>
-                <p class="mt-10 text-xl text-gray-400 leading-8 max-w-4/5">
-                    Here you’ll find the car you’ve always dreamed of — a thrilling and stylish ride
-                    that fits your budget without compromising on quality or value.
-                </p>
+        <div class="max-w-[1500px] m-auto mt-36">
+            <div class="flex gap-5 items-center px-4">
+                <div class="w-1/2">
+                    <h1 class="text-8xl font-extrabold">
+                        Premium
+                        <br />
+                        Car Rental
+                        <br />
+                        In Warsaw
+                    </h1>
+                    <p class="mt-10 text-xl text-gray-400 leading-8 max-w-4/5">
+                        Here you’ll find the car you’ve always dreamed of — a thrilling and stylish
+                        ride that fits your budget without compromising on quality or value.
+                    </p>
 
-                <Button class="mt-10">
-                    <div
-                        class="inline-flex gap-2 justify-center items-center text-lg tracking-widest"
-                    >
-                        {{ t('app.browse_offers') }}
-                        <Icon icon="material-symbols:search-rounded" class="text-xl" />
-                    </div>
-                </Button>
+                    <Button class="mt-10">
+                        <div
+                            class="inline-flex gap-2 justify-center items-center text-lg tracking-widest"
+                        >
+                            {{ t('app.browse_offers') }}
+                            <Icon icon="material-symbols:search-rounded" class="text-xl" />
+                        </div>
+                    </Button>
+                </div>
+                <div class="w-1/2">
+                    <!-- <img src="../../shared/assets/car.png" alt="" /> -->
+                </div>
             </div>
-            <div class="w-1/2">
-                <img src="../../shared/assets/car.png" alt="" />
-            </div>
+
+            <Brands />
         </div>
     </div>
 </template>
---
+++ client/src/shared/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as mercedes } from './logos/mercedes.png';
+export { default as lamborghini } from './logos/lamborghini.png';
+export { default as porsche } from './logos/porsche.png';
+export { default as bmw } from './logos/bmw.png';
