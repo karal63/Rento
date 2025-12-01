657a36b790450b18668d8612716af0cfd1498ddc
leo <kuzmichleva1@gmail.com>
2025-12-01 21:57:49 +0100

    add car page loading and start customizing inputs

--- client/src/shared/config/index.ts
+++ client/src/shared/config/index.ts
@@ -1,2 +1,4 @@
 export { i18n, languages } from './i18n';
 export { axiosInstance } from './axiosInstance';
+export { default as VueTelInput } from './telPicker';
+export { default as VueDatePicker } from './datePicker';
