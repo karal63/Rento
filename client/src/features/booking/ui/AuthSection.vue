57566abc7dd9a87e57d5a36fb741846f9a384382
leo <kuzmichleva1@gmail.com>
2025-12-08 21:47:11 +0100

    disable submit button if inputs are empty

--- client/src/features/booking/ui/AuthSection.vue
+++ client/src/features/booking/ui/AuthSection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
     import { Input, Button } from '@/shared/ui';
     import { Icon } from '@iconify/vue';
-    import { ref } from 'vue';
+    import { computed, ref } from 'vue';
 
     const loginUser = ref({
         email: '',
@@ -28,127 +28,146 @@
             isAuthenticated.value = true;
         }, 2000);
     };
+
+    const isDisabled = computed(() => {
+        if (isAuthLogin.value) return !loginUser.value.email || !loginUser.value.password;
+        else
+            return (
+                !signupUser.value.email ||
+                !signupUser.value.password ||
+                !signupUser.value.name ||
+                !signupUser.value.phoneNumber ||
+                !signupUser.value.secondName ||
+                !passwordRepeat.value
+            );
+    });
 </script>
 
 <template>
     <form @submit.prevent>
-        <h2 class="text-2xl mb-3">Login to continue</h2>
-        <div>
-            <div v-if="!isAuthenticated" class="flex-col gap-2">
-                <div v-if="isAuthLogin" class="flex-col gap-2">
-                    <Input
-                        v-model="loginUser.email"
-                        type="email"
-                        size="medium"
-                        placeholder="Name"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="loginUser.password"
-                        type="password"
-                        size="medium"
-                        placeholder="Password"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                </div>
-
-                <div v-else class="flex-col gap-2">
-                    <Input
-                        v-model="signupUser.name"
-                        size="medium"
-                        placeholder="Name"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="signupUser.secondName"
-                        size="medium"
-                        placeholder="Second name"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="signupUser.email"
-                        type="email"
-                        size="medium"
-                        placeholder="Email addres"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="signupUser.phoneNumber"
-                        type="tel"
-                        size="medium"
-                        placeholder="Phone number"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="signupUser.password"
-                        type="password"
-                        size="medium"
-                        placeholder="Password"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                    <Input
-                        v-model="passwordRepeat"
-                        type="password"
-                        size="medium"
-                        placeholder="Password (repeat)"
-                        :disabled="loading"
-                        class="w-full"
-                    />
-                </div>
-
-                <div v-if="isAuthLogin" class="flex gap-2">
-                    <Button @click="submit" size="sm" :disabled="loading" class="max-w-max">
-                        Login
-                    </Button>
-                    <Button
-                        @click="isAuthLogin = false"
-                        size="sm"
-                        color="transparent"
-                        :disabled="loading"
-                        class="max-w-max border border-main-border"
-                    >
-                        Create an account
-                    </Button>
-                </div>
-
-                <div v-else class="flex gap-2">
-                    <Button @click="submit" size="sm" :disabled="loading" class="max-w-max">
-                        RESERVE
-                    </Button>
-                    <Button
-                        @click="isAuthLogin = true"
-                        size="sm"
-                        color="transparent"
-                        :disabled="loading"
-                        class="max-w-max border border-main-border"
-                    >
-                        already have an account
-                    </Button>
-                </div>
-
-                <div>
-                    <p class="my-2 text-sm text-main-gray">———— or ————</p>
-
-                    <Button size="sm" color="transparent" class="border border-main-border">
-                        <Icon icon="ic:baseline-telegram" class="text-3xl" />
-                    </Button>
-                </div>
+        <div v-if="!isAuthenticated" class="flex-col gap-2">
+            <h2 class="text-2xl mb-3">Login to continue</h2>
+            <div v-if="isAuthLogin" class="flex-col gap-2">
+                <Input
+                    v-model="loginUser.email"
+                    type="email"
+                    size="medium"
+                    placeholder="Name"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="loginUser.password"
+                    type="password"
+                    size="medium"
+                    placeholder="Password"
+                    :disabled="loading"
+                    class="w-full"
+                />
             </div>
 
-            <div
-                v-else
-                class="flex items-center gap-10 bg-primary/10 border border-primary rounded-md py-2 px-4 max-w-max"
-            >
-                <p class="text-lg text-primary">Successfully logged in</p>
-                <Icon icon="material-symbols:check-rounded" class="text-2xl text-primary" />
+            <div v-else class="flex-col gap-2">
+                <Input
+                    v-model="signupUser.name"
+                    size="medium"
+                    placeholder="Name"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="signupUser.secondName"
+                    size="medium"
+                    placeholder="Second name"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="signupUser.email"
+                    type="email"
+                    size="medium"
+                    placeholder="Email addres"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="signupUser.phoneNumber"
+                    type="tel"
+                    size="medium"
+                    placeholder="Phone number"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="signupUser.password"
+                    type="password"
+                    size="medium"
+                    placeholder="Password"
+                    :disabled="loading"
+                    class="w-full"
+                />
+                <Input
+                    v-model="passwordRepeat"
+                    type="password"
+                    size="medium"
+                    placeholder="Password (repeat)"
+                    :disabled="loading"
+                    class="w-full"
+                />
             </div>
+
+            <div v-if="isAuthLogin" class="flex gap-2">
+                <Button
+                    @click="submit"
+                    size="sm"
+                    :disabled="loading || isDisabled"
+                    class="max-w-max"
+                >
+                    Login & proceed
+                </Button>
+                <Button
+                    @click="isAuthLogin = false"
+                    size="sm"
+                    color="transparent"
+                    :disabled="loading"
+                    class="max-w-max border border-main-border"
+                >
+                    Create an account
+                </Button>
+            </div>
+
+            <div v-else class="flex gap-2">
+                <Button
+                    @click="submit"
+                    size="sm"
+                    :disabled="loading || isDisabled"
+                    class="max-w-max"
+                >
+                    CREATE & proceed
+                </Button>
+                <Button
+                    @click="isAuthLogin = true"
+                    size="sm"
+                    color="transparent"
+                    :disabled="loading"
+                    class="max-w-max border border-main-border"
+                >
+                    already have an account
+                </Button>
+            </div>
+
+            <div>
+                <p class="my-2 text-sm text-main-gray">———— or ————</p>
+
+                <Button size="sm" color="transparent" class="border border-main-border">
+                    <Icon icon="ic:baseline-telegram" class="text-3xl" />
+                </Button>
+            </div>
+        </div>
+
+        <div v-else class="flex justify-end">
+            <Button size="sm" class="flex-center gap-3">PROCEED</Button>
         </div>
     </form>
 </template>
+
+<!-- learn more about stripe and create layer that is going to have summary widget in /book/:id and /book/:id/payment -->
