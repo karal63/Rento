68b44fc5a8fc867b38ff5ab0b979a6852ff2f652
leo <kuzmichleva1@gmail.com>
2025-12-20 17:11:36 +0100

    create functional payments

--- client/src/features/payment/ui/PaymentPanel.vue
+++ client/src/features/payment/ui/PaymentPanel.vue
@@ -10,6 +10,13 @@
     import { computed, onMounted, ref } from 'vue';
     import { apiCreatePaymentIntent } from '../api/payment.api';
     import { Icon } from '@iconify/vue';
+    import { calculateRentalPrice } from '@/entities/booking';
+    import { useCarStore } from '@/entities/car';
+    import { useBookingStore } from '@/features/booking';
+    import { useI18n } from 'vue-i18n';
+    const carStore = useCarStore();
+    const bookingStore = useBookingStore();
+    const { t } = useI18n();
 
     const publishableKey =
         'pk_test_51ScRd71aiYRGwgEigAXPwQNOXdaE1KmnXKsECMimAkOUOG3ARc4F62VaFUpySHCp5Z2pwaocyS0y6eCGOODeM3BE00SdO0aVU2';
@@ -45,7 +52,8 @@
     };
 
     onMounted(async () => {
-        const res = await apiCreatePaymentIntent(1000);
+        if (!carStore.selectedCar) return;
+        const res = await apiCreatePaymentIntent(getFullPrice.value * 100);
         clientSecret.value = res.data.clientSecret;
     });
 
@@ -56,6 +64,15 @@
                 theme: 'flat',
             },
         };
+    });
+
+    const getFullPrice = computed(() => {
+        const pricePerDay = calculateRentalPrice(
+            bookingStore.daysCount,
+            carStore.selectedCar!.pricing
+        );
+
+        return pricePerDay * bookingStore.daysCount;
     });
 </script>
 
@@ -71,7 +88,7 @@
                 <VueStripePaymentElement />
 
                 <Button size="md" type="submit" :disabled="loading" class="mt-10 w-full">
-                    {{ loading ? 'Processing...' : `Pay ` }}
+                    {{ loading ? 'Processing...' : `Pay ${getFullPrice}${t('app.zl')}` }}
                 </Button>
             </form>
         </VueStripeElements>
