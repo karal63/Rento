f6b3d4c8b65747053ae3ce164176fddb448057b4
leo <kuzmichleva1@gmail.com>
2026-01-06 17:56:27 +0100

    move logout and admin buttons to account summary widget

--- client/src/app/layouts/ProfileLayout.vue
+++ client/src/app/layouts/ProfileLayout.vue
@@ -1,40 +1,16 @@
 <script setup lang="ts">
-    import { hasPermission } from '@/entities/auth';
-    import { useUserStore } from '@/entities/user';
-    import { logout } from '@/features/auth/logout';
-    import { Button } from '@/shared/ui';
     import { Icon } from '@iconify/vue';
     import { useI18n } from 'vue-i18n';
     import { useRoute } from 'vue-router';
 
     const route = useRoute();
     const { t } = useI18n();
-    const userStore = useUserStore();
 </script>
 
 <template>
     <section class="max-w-[1700px] m-auto 0 px-5 mt-24 sm:mt-32">
-        <div class="flex-between">
+        <div class="flex-col md:flex-between">
             <h1 class="text-3xl font-bold">{{ t('app.user_profile') }}</h1>
-
-            <div class="flex gap-3">
-                <RouterLink
-                    v-if="hasPermission(userStore.user, 'view:admin-page')"
-                    to="/admin/dashboard"
-                >
-                    <Button v-if="userStore.isAuthenticated" size="sm">ADMIN</Button>
-                </RouterLink>
-
-                <Button
-                    v-if="userStore.isAuthenticated"
-                    @click="logout"
-                    size="sm"
-                    color="transparent"
-                    class="border border-red-500 text-red-500 hover:text-red-700 hover:border-red-700 hover:bg-transparent"
-                >
-                    {{ t('app.logout') }}
-                </Button>
-            </div>
         </div>
 
         <ul class="flex items-center gap-5 mt-8 text-lg">
