7668476e486c2d03ff319b5d5fd85058b538291e
leo <kuzmichleva1@gmail.com>
2026-01-26 09:16:38 +0100

    translate admin sidebar links

--- client/src/shared/config/navigation/sidebarLinks.ts
+++ client/src/shared/config/navigation/sidebarLinks.ts
@@ -2,16 +2,16 @@ export const adminLinks = [
     {
         path: '/admin/dashboard',
         icon: 'material-symbols:dashboard-outline-rounded',
-        name: 'Dashboard',
+        name: 'dashboard',
     },
     {
         path: '/admin/rentals',
         icon: 'material-symbols-light:car-rental',
-        name: 'Rentals',
+        name: 'rentals',
     },
     {
         path: '/admin/users',
         icon: 'mdi-light:account',
-        name: 'Users',
+        name: 'users',
     },
 ] as const;
