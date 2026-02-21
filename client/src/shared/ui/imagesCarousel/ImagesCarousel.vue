444e88db00cb4bb93a5013ae3022f7fffb92e53e
leo <kuzmichleva1@gmail.com>
2026-02-21 14:01:31 +0100

    make carousel buttons a bit transparent

--- client/src/shared/ui/imagesCarousel/ImagesCarousel.vue
+++ client/src/shared/ui/imagesCarousel/ImagesCarousel.vue
@@ -33,14 +33,14 @@
 
         <button
             @click="showPreviousImage"
-            class="absolute left-3 top-1/2 transform -translate-y-1/2 rotate-180 w-10 h-10 flex-center bg-main-bg rounded-full cursor-pointer"
+            class="absolute left-3 top-1/2 transform -translate-y-1/2 rotate-180 w-10 h-10 flex-center bg-main-bg/80 rounded-full cursor-pointer"
         >
             <Icon icon="weui:arrow-filled" class="text-2xl" />
         </button>
 
         <button
             @click="showNextImage"
-            class="absolute right-3 top-1/2 transform -translate-y-1/2 w-10 h-10 flex-center bg-main-bg rounded-full cursor-pointer"
+            class="absolute right-3 top-1/2 transform -translate-y-1/2 w-10 h-10 flex-center bg-main-bg/80 rounded-full cursor-pointer"
         >
             <Icon icon="weui:arrow-filled" class="text-2xl" />
         </button>
